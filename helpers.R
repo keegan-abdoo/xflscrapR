aa6ef14f6faadce532368cb92ed437cfb48bc1b1
keegan-abdoo <51758907+keegan-abdoo@users.noreply.github.com>
2020-02-18 09:09:28 -0800

    Update helpers.R

--- helpers.R
+++ helpers.R
@@ -4,3 +4,11 @@ mins_to_seconds <- function(mins_str){
   seconds <- as.numeric(unlist(strsplit(mins_str,":"))[2])
   return(mins * 60 + seconds)
 }
+
+# Helper Function for converting field position to distance from endzone
+yl_100 <- Vectorize(function(fp_string, off){
+    case_when(str_detect(fp_string, "50$") ~ 50,
+              str_extract(fp_string, "[A-Z]+(?= [0-9]{1,2}$)") == off ~ 
+                  100 - as.numeric(str_extract(fp_string, "[0-9]{1,2}$")),
+              TRUE ~ as.numeric(str_extract(fp_string, "[0-9]{1,2}$")))
+})
