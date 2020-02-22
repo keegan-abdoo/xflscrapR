b4471e88a11d6c51577d780add17a70adb1ffd8c
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-02-21 20:46:22 -0500

    Adding helper function to conveniently get team info (colors, logos, city, etc)

--- helpers.R
+++ helpers.R
@@ -12,3 +12,16 @@ yl_100 <- Vectorize(function(fp_string, off){
                   100 - as.numeric(str_extract(fp_string, "[0-9]{1,2}$")),
               TRUE ~ as.numeric(str_extract(fp_string, "[0-9]{1,2}$")))
 })
+
+# get colors logos from xflscrapR github and team info from Anthony Reinhard's github
+get_team_info <- function() {
+  d1 <- read.csv(url("https://raw.githubusercontent.com/ajreinhard/XFL-public/master/teams.csv")) %>%
+    select(Abbr, City, TeamName, Full, HeadCoach, Division, Helm)
+  d2 <- read.csv(url("https://raw.githubusercontent.com/keegan-abdoo/xflscrapR/master/xfl_logos_and_colors.csv")) %>%
+    select(ClubCode, logo, primary, secondary, tertiary, quarternary)
+  
+  xfl_colors <- d2 %>% left_join(d1, by = c("ClubCode" = "Abbr")) %>%
+    rename(team_code = ClubCode, url = logo)
+  
+  return(xfl_colors)
+}
